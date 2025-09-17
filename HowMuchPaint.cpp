70fae6de5e9ccee6f4f8b9aa10900d473c0ec072
tucke <tucke@DESKTOP-COAP6VQ>
2025-09-17 17:34:48 -0400

    Add project files.

---
+++ HowMuchPaint.cpp
@@ -0,0 +1,37 @@
+// HowMuchPaint.cpp : The purpose is to calculate how many gallons of paint to coat a 6ft high and 100ft long fence twice
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{ // variables for my input
+    const float paintcov = 340; // sqft per gallon
+    float height = 6,
+        length = 100,
+
+        // process
+        area = height * length,
+        totalArea = area * 2,
+        gallonsNeeded = totalArea / paintcov;
+        
+    // rounding to the nearest whole number
+    cout.setf(ios::fixed);
+    cout.precision(0);
+
+      // output
+    cout << endl
+        << "Height " << height << endl
+        << "Length " << length << endl
+        << "Total Area " << totalArea << endl
+        << "Coverage per Gallon " << paintcov << endl
+        << "Gallons Needed " << gallonsNeeded << endl
+        << endl;
+      
+
+
+
+        return 0;
+}
+
+
